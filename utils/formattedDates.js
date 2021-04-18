65588f852b3839544438918a2e892c8dd1c1df73
ravioli-j-code <53997832+ravioli-j-code@users.noreply.github.com>
2021-04-18 11:46:35 +0900

    Update formattedDates.js

--- utils/formattedDates.js
+++ utils/formattedDates.js
@@ -1,40 +1,55 @@
-function GetFormattedDate(fm) {
+function GetFormattedDate(fm, browserType) {
 
-    let format = !fm ?  '' : fm;
-    let date = new Date();
-    let options = {
-        timeZone: 'Asia/Seoul',
-        year: 'numeric',
-        month: '2-digit',
-        day: '2-digit'
-    };
-    let d = date.toLocaleDateString('ko-KR', options);
-    let dateArr = d.split(".");
-    let resultArr = [];
+    const format = !fm ?  '' : fm;
+    const date = new Date();
+    
+    const getChromeDates = function() {
+        
+        const options = {
+            timeZone: 'Asia/Seoul',
+            year: 'numeric',
+            month: '2-digit',
+            day: '2-digit'
+        };
+        
+        const d = date.toLocaleDateString('ko-KR', options);
+        const dateArr = d.split(".");
+        let resultArr = [];
 
-    for (var i = 0 ; i < dateArr.length ; i++ ) {
-        let data = dateArr[i] ? dateArr[i].replace(/\s/ig, '') : '';
-        if (data) {
-            resultArr.push(data.length < 2 ? '0'+ data : data);
+        for (var i = 0 ; i < dateArr.length ; i++ ) {
+            let data = dateArr[i] ? dateArr[i].replace(/\s/ig, '') : '';
+            if (data) {
+                resultArr.push(data.length < 2 ? '0'+ data : data);
+            }
         }
+        return resultArr.join(format);
+    };
+    
+    const getIEDates = function() {
+        
+        let year = date.getYear() + 1900;
+        let month = date.getMonth() + 1;
+        
+        
     }
-    return resultArr.join(format);
+    
 }
 
 
-function GetFormattedTime(fm) {
+function GetFormattedTime(fm, browserType) {
 
-    let format = !fm ?  ':' : fm;
-    let date = new Date();
-    let options = {
+    const format = !fm ?  ':' : fm;
+    const date = new Date();
+    const options = {
         timeZone: 'Asia/Seoul',
         hour12: false,
         hour: '2-digit',
         minute: '2-digit',
         second: '2-digit'
     };
-    let d = date.toLocaleTimeString('ko-KR', options);
-    let dateArr = d.split(":");
+    
+    const d = date.toLocaleTimeString('ko-KR', options);
+    const dateArr = d.split(":");
     let resultArr = [];
 
     for (let i = 0 ; i < dateArr.length ; i++ ) {
